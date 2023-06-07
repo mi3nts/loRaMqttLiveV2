df535946c184cc2b357585effaab80a3179e5b88
lakihaomal <lhw150030@utdallas.edu>
2023-06-06 22:42:35 -0500

    new pickle folders v3

--- firmware/mintsXU4/mintsProcessing.py
+++ firmware/mintsXU4/mintsProcessing.py
@@ -329,31 +329,6 @@ def getDataSuperReader(nodeID,sensorID,beginDate):
     return mintsData
 
 
-def climateDataPrepV3(nodeData,nodeID,WIMDA,YXXDR):
-
-    climateData = getDataSuperReader(nodeID,nodeData['climateSensor'],nodeData['climateSensorBegin'])
-    gpsData     = getDataSuperReader(nodeID,nodeData['gpsSensor'],nodeData['gpsSensorBegin'])
-    mintsData   = merger([climateData, WIMDA,YXXDR, gpsData])
-
-    if len(mintsData.index)>0:
-        print("GPS Cropping")
-        pd.to_pickle(mintsData,getPathGeneric(mergedPklsFolder,nodeID,"climateData","pkl") )
-        
-        mintsData = gpsCropCoordinatesV2(mintsData,nodeData['gpsSensor'],32.992179, -96.757777,0.0015,0.0015)
-        
-        pd.to_pickle(mintsData,getPathGeneric(mergedPklsFolder,nodeID,"climateDataWSTCV3","pkl") )
-        pd.to_pickle(mintsData.dropna(),getPathGeneric(mergedPklsFolder,nodeID,"climateDataWSTCCurrentV3","pkl") )
-
-        print("-----------------------------------------------")
-        print(mintsData)
-        print("-----------------------------------------------")
-
-    else:
-        print("-----------------------------------------------")
-        print("No data for Node ID: "+ nodeID)
-        print("-----------------------------------------------")
-
-
 def climateDataPrepV2(nodeData,nodeID,WIMDA,YXXDR):
 
     climateData = getDataSuperReader(nodeID,nodeData['climateSensor'],nodeData['climateSensorBegin'])
