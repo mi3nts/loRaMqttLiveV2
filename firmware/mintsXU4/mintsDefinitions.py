635b8969acadb3e620d12d2aa782bf5af3ba3a7f
lakihaomal <lhw150030@utdallas.edu>
2023-06-07 01:04:37 -0500

    initial updates

--- firmware/mintsXU4/mintsDefinitions.py
+++ firmware/mintsXU4/mintsDefinitions.py
@@ -50,7 +50,8 @@ rawPklsFolder             = dataFolder    + "/rawPklsV3"
 referencePklsFolder       = dataFolder    + "/referencePklsV3"
 mergedPklsFolder          = dataFolder    + "/mergedPklsV3"
 modelsPklsFolder          = dataFolder    + "/modelsPklsV3"
-liveFolder                = dataFolder    + "/liveUpdate/resultsV3"
+# liveFolder                = dataFolder    + "/liveUpdate/results"
+liveFolder                = dataFolder    + "/testNow"
 
 
 # Change Accordingly  
